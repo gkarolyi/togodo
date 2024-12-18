c83b4793b4d2c158249b22ce2f0d646c7f26fbad
gkarolyi <33122882+gkarolyi@users.noreply.github.com>
2024-12-19 09:51:02 +1100

    hook up Add command

--- cmd/add.go
+++ cmd/add.go
@@ -1,6 +1,5 @@
 /*
 Copyright © 2024 NAME HERE <EMAIL ADDRESS>
-
 */
 package cmd
 
@@ -22,10 +21,14 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("add called")
-		repo := todolib.TodoRepository{}
-		repo.ReadFile("../testdata/todo.txt")
-		todo := repo.Add("(B) here's another important todo +togodo")
+		repo, err := todolib.New(TodoTxtPath)
+		if err != nil {
+			fmt.Println(err)
+		}
+		todo, err := repo.Add(args[0])
+		if err != nil {
+			fmt.Println(err)
+		}
 		fmt.Println(todo.Text)
 	},
 }
